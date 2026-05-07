7b205e2a8be8b6056ef8ca27d0ae9dfb3b76d241
SQMUSIC <sqthemusic@gmail.com>
2026-05-08 00:27:30 +0300

    Create i18n.js

---
+++ src/src/i18n.js
@@ -0,0 +1,64 @@
+export const translations = {
+  en: {
+    home: "Home",
+    blog: "Blog",
+    about: "About",
+    shop: "Shop",
+    contact: "Contact",
+
+    shop_title: "Shop",
+    shop_text: "My store is below",
+
+    contact_title: "Contact",
+    contact_name: "Name",
+    contact_email: "Email",
+    contact_message: "Message",
+    contact_send: "Send",
+
+    footer_nav: "Navigation",
+    footer_support: "Support",
+    footer_social: "Social"
+  },
+
+  fr: {
+    home: "Accueil",
+    blog: "Blog",
+    about: "À propos",
+    shop: "Boutique",
+    contact: "Contact",
+
+    shop_title: "Boutique",
+    shop_text: "Ma boutique est ci-dessous",
+
+    contact_title: "Contact",
+    contact_name: "Nom",
+    contact_email: "Email",
+    contact_message: "Message",
+    contact_send: "Envoyer",
+
+    footer_nav: "Navigation",
+    footer_support: "Support",
+    footer_social: "Réseaux"
+  },
+
+  ru: {
+    home: "Главная",
+    blog: "Блог",
+    about: "О нас",
+    shop: "Магазин",
+    contact: "Контакты",
+
+    shop_title: "Магазин",
+    shop_text: "Мой магазин ниже",
+
+    contact_title: "Контакт",
+    contact_name: "Имя",
+    contact_email: "Почта",
+    contact_message: "Сообщение",
+    contact_send: "Отправить",
+
+    footer_nav: "Навигация",
+    footer_support: "Поддержка",
+    footer_social: "Соцсети"
+  }
+};
